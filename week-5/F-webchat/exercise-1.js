bab70e2ebf6dee4f097cdc14a6a655e77fa3a877
Ahmed Merehil <ahmed8merehil@yahoo.com>
2019-04-24 13:03:27 +0100

    week-5-chatbox

--- week-5/F-webchat/exercise-1.js
+++ week-5/F-webchat/exercise-1.js
@@ -34,3 +34,66 @@ When you open index.html in your browser, it should display the existing message
 */
 
 // Write your code here
+
+fetch("https://codeyourfuture.herokuapp.com/api/messages").then(function(
+  response
+) {
+  return response.text();
+});
+
+var messages = document.getElementById("message-list");
+var textbox = document.getElementById("message-input");
+var subButton = document.getElementById("submit");
+
+subButton.addEventListener("click", function() {
+  var newMessage = document.createElement("messagelist");
+  newMessage.innerHTML = textbox.value;
+  messages.appendChild(newMessage);
+  alert("Thanks for Your Messege");
+  textbox.value = "";
+});
+
+//   $(document).ready(function () {
+//     $('#submit').on('click', function () {
+//         var name = $('#name').val();
+//         var shout = $('#shout').val();
+//         var date = getDate();
+//         var dataString = 'name='+name+'&shout='+shout+'&date'+date;
+
+//         //Validation
+//         if(name == '' || shout == ''){
+//             alert('Please Fill in Your Name & Messege');
+//         }
+//         else{
+//             $.ajax({
+//                 type:"POST",
+//                 url:"../shoutbox/shoutbox.php",
+//                 data:dataString,
+//                 cache:false,
+//                 success: function (html){
+//                     $('#shouts ul').prepend(html);
+//                 }
+//             });
+//         }
+
+//         return false;
+
+//     });
+
+//     $('#shout').focusin(function () {
+//        $(this).val("");
+//     });
+// });
+
+// function getDate(){
+//     var date;
+//     date = new Date;
+//     date = date.getUTCFullYear() + '-' +
+//         ('00' + (date.getUTCMonth() + 1)).slice(-2)+'-'+
+//         ('00' + date.getUTCDate()).slice(-2)+' '+
+//         ('00' + date.getUTCHours()).slice(-2)+':'+
+//         ('00' + date.getUTCMinutes()).slice(-2)+':'+
+//         ('00' + date.getUTCSeconds()).slice(-2);
+
+//     return date;
+// }
